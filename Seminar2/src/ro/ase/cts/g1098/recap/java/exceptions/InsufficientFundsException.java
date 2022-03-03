b4f4dc82a1ec815b83499eed39418c0482e7cfde
Cristiana Jipa <jipacristiana@gmail.com>
2022-03-03 20:04:09 +0200

    initial commit Seminar 2

---
+++ Seminar2/src/ro/ase/cts/g1098/recap/java/exceptions/InsufficientFundsException.java
@@ -0,0 +1,12 @@
+package ro.ase.cts.g1098.recap.java.exceptions;
+
+public class InsufficientFundsException extends Exception{
+	
+	public InsufficientFundsException(String msg) {
+		super(msg);
+	}
+	
+	public InsufficientFundsException() {
+		super(); //not mandatory
+	}
+}
---
+++ Seminar2/src/ro/ase/cts/g1098/recap/java/models/Profitable.java
@@ -0,0 +1,8 @@
+package ro.ase.cts.g1098.recap.java.models;
+
+public interface Profitable {
+	
+	//10%
+	//0.1
+	public abstract void addInterest(double interestPercentage);
+}

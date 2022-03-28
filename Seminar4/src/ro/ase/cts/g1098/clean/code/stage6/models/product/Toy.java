90ef9813922a3d2ca8247e8e557cc4d2c6eeee00
Cristiana Jipa <jipacristiana@gmail.com>
2022-03-28 10:46:58 +0300

    seminar 5

---
+++ Seminar4/src/ro/ase/cts/g1098/clean/code/stage6/models/product/Toy.java
@@ -0,0 +1,11 @@
+package ro.ase.cts.g1098.clean.code.stage6.models.product;
+
+public class Toy extends Product{
+	
+	int minAge;
+	String description;
+	
+	//OPEN CLOSE PRINCIPLE
+	//we can choose to override some of the product methods or not
+
+}
---
+++ Seminar5/src/ro/ase/csie/cts/g1098/design/patterns/singleton/DBConnection.java
@@ -0,0 +1,34 @@
+package ro.ase.csie.cts.g1098.design.patterns.singleton;
+
+public class DBConnection {
+	
+	//all should be private!
+	private static DBConnection connection;
+	
+	String iPAddress;
+	String userName;
+	String userPassword;
+	
+	//change from public to private!!!!
+	private DBConnection(String iPAddress, String userName, String userPassword) {
+		super();
+		this.iPAddress = iPAddress;
+		this.userName = userName;
+		this.userPassword = userPassword;
+	}
+	
+	
+	private DBConnection() {
+		
+	}
+	
+	
+	public static DBConnection getInstance() {
+		if(connection == null) {
+			connection = new DBConnection();
+		}
+		
+		return connection;
+	}
+
+}

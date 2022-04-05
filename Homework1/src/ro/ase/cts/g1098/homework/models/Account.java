a0c9537a12d7847859912655e937f06f9f476632
Cristiana Jipa <jipacristiana@gmail.com>
2022-04-05 21:39:09 +0300

    Stage 6 - appling the SOLID Single Responsability Principle

--- Homework1/src/ro/ase/cts/g1098/homework/models/Account.java
+++ Homework1/src/ro/ase/cts/g1098/homework/models/Account.java
@@ -2,9 +2,14 @@ package ro.ase.cts.g1098.homework.models;
 
 public class Account {
 	
-	public double loanValue, rate;	
+	public double loanValue;
+	public double rate;	
 	public int daysActive;
-	AccountType accountType;
+	public AccountType accountType;
+
+	
+	public static final float BROKER_FEE = 0.0125f;
+	public static final int DAYS_YEAR = 365;
 	
 	public Account(double loanValue, double rate, AccountType accountType) throws Exception {
 		if (loanValue < 0)
@@ -15,33 +20,84 @@ public class Account {
 		this.accountType = accountType;
 	}
 	
-	public static double calculateTotalFee(Account[] accounts) {
+	public static double computeTotalFee(Account[] accounts) {
 		Account	account;
 		double totalFee = 0.0;
 		
 		for	(int i= 0; i < accounts.length; i++) {
 			account = accounts[i];
-			if(account.accountType == AccountType.PREMIUM || account.accountType == AccountType.SUPER_PREMIUM)	
-				totalFee += .0125	*	//	1.25%	broker's	fee
-				(account.loanValue * Math.pow(account.rate,(account.daysActive/365)) - account.loanValue);	//	interest-principal
+			if(account.accountType == AccountType.PREMIUM || account.accountType == AccountType.SUPER_PREMIUM) {
+				double annualRate = account.computeAnnualRate(account.getRate(), account.getDaysActive());
+				double loanFee = account.computeLoanFee(account.getLoanValue(), annualRate);
+				double loanFeeWithTax = account.computeLoanFeeExtraTax(BROKER_FEE, loanFee);
+
+				totalFee += loanFeeWithTax;
+			}
+				
 		}
 		return	totalFee;
 	}
 	
+	
+	public double computeAnnualRate(double rate, double daysActive) {
+		return Math.pow(getRate(), getDaysActive() / DAYS_YEAR);
+	}
+	
+	public double computeLoanFee(double loanValue, double annualRate) {
+		return (loanValue * annualRate) - loanValue;
+	}
+	
+	public double computeLoanFeeExtraTax (double extraTax, double loanFee) {
+		if (extraTax <= 0)
+			loanFee *= extraTax;
+		return loanFee;
+	}
+	
+	
+	
+	
+	
 	public double getLoanValue() {
-		System.out.println("The loan value is " + this.loanValue);
 		return loanValue;
 	}
 	
+	public void showLoan() {
+		System.out.println("The loan value is " + this.loanValue);
+	}
+	
+	
 	public double getRate() {
-		System.out.println("The rate is " + rate);
 		return this.rate;
 	}
+	
+	public void showRate() {
+		System.out.println("The rate is " + rate);
+	}
+	
+	public double getDaysActive() {
+		return daysActive;
+	}
+	
+	public void showDaysActive() {
+		System.out.println("The active days are " + this.daysActive);
+	}
+	
+	public AccountType getAccountType() {
+		return accountType;
+	}
+
+	public void showAccount() {
+		System.out.println("This is an account");
+	}
+
+	
 	
 	//must have method - the lead has requested it in all classes
 	public double getMonthlyRate() {
 		return loanValue*rate;
 	}
+	
+	
 	
 	public void setLoanValue(double loanValue) throws Exception {
 		if(loanValue<0)
@@ -56,10 +112,7 @@ public class Account {
 				+ "days active:" + daysActive + "; Type: " + accountType + ";";
 	}
 	
-	public void printAccount() {
-		
-		System.out.println("This is an account");
-	}
+	
 
 	
 
